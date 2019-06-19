8849d7f5dc3528c086d8957b427bc942d9bf105d
Luis G <lg2696@hotmail.com>
2019-06-19 14:43:23 -0500

    se agrego un servidor local para la version de produccion

--- chat/src/app/services/api.ts
+++ chat/src/app/services/api.ts
@@ -1,4 +1,5 @@
-const ip = 'localhost';
-const port = 8000;
+// const ip = 'localhost';
+const ip = '192.168.0.5';
+const port = 3000;
 export const API_URL = `${ip}:${port}`;
 export const URI = `http://${API_URL}`;
