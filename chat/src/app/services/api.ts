b0b6b746d4f85128a23ddd04aae51c3050d57e23
Luis G <lg2696@hotmail.com>
2019-06-19 11:36:46 -0500

    se cambio el puerto de la api

--- chat/src/app/services/api.ts
+++ chat/src/app/services/api.ts
@@ -1,4 +1,4 @@
 const ip = 'localhost';
-const port = 3000;
+const port = 8000;
 export const API_URL = `${ip}:${port}`;
-export const URI = `http://${ip}:${port}`;
+export const URI = `http://${API_URL}`;
