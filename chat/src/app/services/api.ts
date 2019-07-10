878c92917d45fac4e8902f084628b67564aee438
Luis G <lg2696@hotmail.com>
2019-07-10 18:25:39 -0500

    :v

--- chat/src/app/services/api.ts
+++ chat/src/app/services/api.ts
@@ -1,5 +1,5 @@
 // const ip = 'localhost';
-const ip = '192.168.0.5';
+const ip = '192.168.43.54';
 const port = 3000;
 export const API_URL = `${ip}:${port}`;
 export const URI = `http://${API_URL}`;
